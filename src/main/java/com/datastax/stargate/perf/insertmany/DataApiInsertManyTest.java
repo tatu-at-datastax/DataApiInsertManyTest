92ff76032dba0a6a66e030a058845605ac7c2754
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-04-05 16:30:10 -0700

    ...

--- src/main/java/com/datastax/stargate/perf/insertmany/DataApiInsertManyTest.java
+++ src/main/java/com/datastax/stargate/perf/insertmany/DataApiInsertManyTest.java
@@ -3,14 +3,34 @@ package com.datastax.stargate.perf.insertmany;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 
-import com.datastax.astra.client.DataAPIClient;
-import com.datastax.astra.client.Database;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
+import com.datastax.astra.client.DataAPIClient;
+import com.datastax.astra.client.DataAPIOptions;
+import com.datastax.astra.client.Database;
+import com.dtsx.astra.sdk.db.exception.DatabaseNotFoundException;
+
 public class DataApiInsertManyTest implements Callable<Integer>
 {
+	enum DataApiEnv {
+		PROD(DataAPIOptions.DataAPIDestination.ASTRA),
+		DEV(DataAPIOptions.DataAPIDestination.ASTRA_DEV),
+		TEST(DataAPIOptions.DataAPIDestination.ASTRA_TEST),
+		LOCAL(DataAPIOptions.DataAPIDestination.DSE)
+		;
+		
+		private final DataAPIOptions.DataAPIDestination destination;
+
+		private DataApiEnv(DataAPIOptions.DataAPIDestination d) {
+			destination = d;
+		}
+
+		public DataAPIOptions.DataAPIDestination destination() {
+			return destination;
+		}
+	}
+	
 	private final static String TOKEN_PREFIX = "AstraCS:";
 
 	@Option(names = {"-t", "--token"}, required=true,
@@ -18,16 +38,20 @@ public class DataApiInsertManyTest implements Callable<Integer>
 	private String astraToken;
 
 	@Option(names = {"-i", "--db-id"}, required=true,
-			description = "Database Id (UUID)")
+			description = "Database ID (UUID)")
 	private String dbIdAsString;
+
+	@Option(names = {"-e", "--env"}, required=false,
+			description = "Astra env (PROD [default], DEV, TEST, LOCAL)")
+	private DataApiEnv env = DataApiEnv.PROD;
 
 	@Option(names = {"-r", "--db-region"}, required=false,
 			description = "Database region (like 'us-east1')")
 	private String dbRegion = "us-east1";
 
 	@Option(names = {"-k", "--keyspace"}, required=false,
-			description = "Keyspace (like 'ks')")
-	private String keyspace = "default";
+			description = "Keyspace (like 'ks'; default 'default_keyspace')")
+	private String keyspace = "default_keyspace";
 
 	
 	@Override
@@ -45,14 +69,31 @@ public class DataApiInsertManyTest implements Callable<Integer>
 			return 2;
 		}
 		System.out.print("Creating DataAPIClient...");
-		DataAPIClient client = new DataAPIClient(astraToken);
+		DataAPIClient client = new DataAPIClient(astraToken,
+				DataAPIOptions.builder()
+				.withDestination(env.destination())
+				.build());
 		client.getDatabase(astraToken);
 		System.out.println(" created");
 
-		System.out.printf("Connecting to database '%s'...", dbId);
-		Database db = client.getDatabase(dbId);
+		System.out.printf("Connecting to database '%s' (env '%s')...",
+				dbId, env.name());
+		Database db;
+
+		try {
+			db = client.getDatabase(dbId);
+		} catch (DatabaseNotFoundException dbNfe) {
+			System.err.printf(" FAIL: (%s) %s\n", dbNfe.getClass().getSimpleName(),
+					dbNfe.getMessage());
+			return 3;
+		}
 		System.out.printf(" connected: namespace '%s'\n", db.getNamespaceName());
 
+		System.out.printf("Collections in the Database/namespace: ");
+		System.out.println(db.listCollectionNames().toList());
+
+		System.out.println("DONE!");
+		
 		return 0;
 	}
 	
