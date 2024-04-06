4ab85d4bfde39ac37ef67f36e61b7dc0bb68d4ae
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-04-05 17:47:11 -0700

    ...

--- src/main/java/com/datastax/stargate/perf/insertmany/DataApiInsertManyTest.java
+++ src/main/java/com/datastax/stargate/perf/insertmany/DataApiInsertManyTest.java
@@ -14,102 +14,106 @@ import com.dtsx.astra.sdk.db.exception.DatabaseNotFoundException;
 
 public class DataApiInsertManyTest implements Callable<Integer>
 {
-	enum DataApiEnv {
-		PROD(DataAPIOptions.DataAPIDestination.ASTRA),
-		DEV(DataAPIOptions.DataAPIDestination.ASTRA_DEV),
-		TEST(DataAPIOptions.DataAPIDestination.ASTRA_TEST),
-		LOCAL(DataAPIOptions.DataAPIDestination.DSE)
-		;
+    enum DataApiEnv {
+        PROD(DataAPIOptions.DataAPIDestination.ASTRA),
+        DEV(DataAPIOptions.DataAPIDestination.ASTRA_DEV),
+        TEST(DataAPIOptions.DataAPIDestination.ASTRA_TEST),
+        LOCAL(DataAPIOptions.DataAPIDestination.DSE)
+        ;
 		
-		private final DataAPIOptions.DataAPIDestination destination;
+        private final DataAPIOptions.DataAPIDestination destination;
 
-		private DataApiEnv(DataAPIOptions.DataAPIDestination d) {
-			destination = d;
-		}
+        private DataApiEnv(DataAPIOptions.DataAPIDestination d) {
+            destination = d;
+        }
 
-		public DataAPIOptions.DataAPIDestination destination() {
-			return destination;
-		}
-	}
+        public DataAPIOptions.DataAPIDestination destination() {
+            return destination;
+        }
+    }
 	
-	private final static String TOKEN_PREFIX = "AstraCS:";
+    private final static String TOKEN_PREFIX = "AstraCS:";
 
-	@Option(names = {"-t", "--token"}, required=true,
-			description = "Astra Token (starts with 'AstraCS:')")
-	private String astraToken;
+    @Option(names = {"-t", "--token"}, required=true,
+            description = "Astra Token (starts with 'AstraCS:')")
+    private String astraToken;
 
-	@Option(names = {"-i", "--db-id"}, required=true,
-			description = "Database ID (UUID)")
-	private String dbIdAsString;
+    @Option(names = {"-d", "--db", "--db-id"}, required=true,
+            description = "Database ID (UUID)")
+    private String dbIdAsString;
 
-	@Option(names = {"-e", "--env"}, required=false,
-			description = "Astra env (PROD [default], DEV, TEST, LOCAL)")
-	private DataApiEnv env = DataApiEnv.PROD;
+    @Option(names = {"-e", "--env"}, required=false,
+            description = "Astra env (PROD [default], DEV, TEST, LOCAL)")
+    private DataApiEnv env = DataApiEnv.PROD;
 
-	@Option(names = {"-n", "--namespace"}, required=false,
-			description = "Namespace (like 'ks')")
-	private String namespace = null;
+    @Option(names = {"-n", "--namespace"}, required=false,
+            description = "Namespace (like 'ks')")
+    private String namespace = null;
 
-	
-	@Override
-	public Integer call() throws Exception {
-		if (!astraToken.startsWith(TOKEN_PREFIX)) {
-			System.err.printf("Token does not start with prefix '%s': %s\n",
-					TOKEN_PREFIX, astraToken);
-			return 1;
-		}
-		UUID dbId;
-		try {
-			dbId = UUID.fromString(dbIdAsString);
-		} catch (Exception e) {
-			System.err.printf("Database id not valid UUID: %s\n", dbIdAsString);
-			return 2;
-		}
-		System.out.print("Creating DataAPIClient...");
-		DataAPIClient client = new DataAPIClient(astraToken,
-				DataAPIOptions.builder()
-				.withDestination(env.destination())
-				.build());
-		System.out.println(" created");
+    @Option(names = {"-v", "--vector"}, required=false,
+            description = "Vector size; 0 to disable (default: 1536)")
+    private int vectorLength = 1536;
+    
 
-		System.out.printf("Connecting to database '%s' (env '%s')...",
-				dbId, env.name());
-		Database db;
+    @Override
+    public Integer call() throws Exception {
+        if (!astraToken.startsWith(TOKEN_PREFIX)) {
+            System.err.printf("Token does not start with prefix '%s': %s\n",
+                    TOKEN_PREFIX, astraToken);
+            return 1;
+        }
+        UUID dbId;
+        try {
+            dbId = UUID.fromString(dbIdAsString);
+        } catch (Exception e) {
+            System.err.printf("Database id not valid UUID: %s\n", dbIdAsString);
+            return 2;
+        }
+        System.out.print("Creating DataAPIClient...");
+        DataAPIClient client = new DataAPIClient(astraToken,
+                DataAPIOptions.builder()
+                .withDestination(env.destination())
+                .build());
+        System.out.println(" created");
 
-		try {
-			if (namespace == null || namespace.isEmpty()) {
-				db = client.getDatabase(dbId);
-			} else {
-				db = client.getDatabase(dbId, namespace);
-			}
-		} catch (DatabaseNotFoundException dbNfe) {
-			System.err.printf(" FAIL: (%s) %s\n", dbNfe.getClass().getSimpleName(),
-					dbNfe.getMessage());
-			return 3;
-		}
-		System.out.printf(" connected: namespace '%s'\n", db.getNamespaceName());
+        System.out.printf("Connecting to database '%s' (env '%s')...",
+                dbId, env.name());
+        Database db;
 
-		System.out.printf("Fetch Collections in the Database: ");
+        try {
+            if (namespace == null || namespace.isEmpty()) {
+                db = client.getDatabase(dbId);
+            } else {
+                db = client.getDatabase(dbId, namespace);
+            }
+        } catch (DatabaseNotFoundException dbNfe) {
+            System.err.printf(" FAIL: (%s) %s\n", dbNfe.getClass().getSimpleName(),
+                    dbNfe.getMessage());
+            return 3;
+        }
+        System.out.printf(" connected: namespace '%s'\n", db.getNamespaceName());
 
-		Stream<String> collectionNames;
+        System.out.printf("Fetch Collections in the Database: ");
 
-		try {
-			collectionNames	= db.listCollectionNames();
-			System.out.println(collectionNames.toList());
-		} catch (Exception e) {
-			System.err.printf(" FAIL: (%s) %s\n", e.getClass().getSimpleName(),
-					e.getMessage());
-			return 3;
-		}
+        Stream<String> collectionNames;
 
-		System.out.println("DONE!");
-		
-		return 0;
-	}
-	
-	public static void main(String[] args)
+        try {
+            collectionNames	= db.listCollectionNames();
+            System.out.println(collectionNames.toList());
+        } catch (Exception e) {
+            System.err.printf(" FAIL: (%s) %s\n", e.getClass().getSimpleName(),
+                    e.getMessage());
+            return 3;
+        }
+
+        System.out.println("DONE!");
+
+        return 0;
+    }
+
+    public static void main(String[] args)
     {
-		int exitCode = new CommandLine(new DataApiInsertManyTest()).execute(args);
+        int exitCode = new CommandLine(new DataApiInsertManyTest()).execute(args);
         System.exit(exitCode);
-	}
+    }
 }
