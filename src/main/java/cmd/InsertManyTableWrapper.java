2d6a3ef2d92f82beea63ea8620b4e42db8a85bb3
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-11-23 18:54:50 -0800

    Add wrapper for (incomplete) table test

---
+++ src/main/java/cmd/InsertManyTableWrapper.java
@@ -0,0 +1,9 @@
+package cmd;
+
+import com.datastax.stargate.perf.insertmany.InsertManyTableTest;
+
+public class InsertManyTableWrapper {
+    public static void main(String[] args) {
+        InsertManyTableTest.main(args);
+    }
+}
