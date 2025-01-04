ab62ca5437854f4fc1d6b435d2a9f6df568f8c20
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-01-03 17:38:35 -0800

    Add skeletal beginnings of CQL driver based test

--- src/main/java/com/datastax/stargate/perf/base/DataApiCQLTestBase.java
+++ src/main/java/com/datastax/stargate/perf/base/DataApiCQLTestBase.java
@@ -1,11 +1,18 @@
 package com.datastax.stargate.perf.base;
 
+import com.datastax.astra.client.core.options.DataAPIClientOptions;
 import picocli.CommandLine;
 
-public abstract class DataApiCQLTestBase extends DataApiTestBase
-{
+public abstract class DataApiCQLTestBase extends DataApiTestBase {
     @CommandLine.Option(names = {"-T", "--table-name"},
             defaultValue = "insert_many_test_cql_table",
             description = "Table name (default: 'insert_many_test_cql_table')")
     protected String tableName;
+
+    @Override
+    protected final DataAPIClientOptions dataApiOptions(
+            DataAPIClientOptions builder) {
+        throw new IllegalStateException("Should never get called on " + getClass().getName());
+    }
 }
+
